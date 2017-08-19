76aa2ed97344a6d1dad1e4944f1565c3a2e30ccb
JTP709 <JTP709@gmail.com>
2017-08-19 16:54:55 -0400

    expanded dashboard route

--- simple_safety.py
+++ simple_safety.py
@@ -12,10 +12,32 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
+def connect(database_name="safety"):
+    """Connects to database"""
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print ("<error message>")
+
 @app.route('/')
 @app.route('/dashboard/')
 def dashboard():
-	return "Main Dashboard Page"
+	"""Loads the dashboard page"""
+	db, cursor = connect()
+    query = """
+            SELECT articles.title, count(*) as num
+                FROM log JOIN articles
+                ON log.path = concat('/article/', articles.slug)
+                GROUP BY articles.title
+                ORDER BY num desc
+                LIMIT 3;
+            """
+    cursor.execute(query)
+    results = cursor.fetchall()
+    return render_template('dasbhoard.html'query = query)
+    db.close()
 
 @app.route('/reports/')
 def dashboard():
