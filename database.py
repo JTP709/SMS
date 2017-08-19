391a11914ab8bf1e8264eef35713592ace426a12
Jon Prell <JTP709@gmail.com>
2017-08-19 14:46:08 -0400

    updated connect params database name

--- database.py
+++ database.py
@@ -49,7 +49,7 @@ def create_tables():
         # read the connection parameters
         params = config()
         # connect to the PostgreSQL server
-        conn = psycopg2.connect("dbnname = safety user = postgres password = postgres")
+        conn = psycopg2.connect("database = safety user = postgres password = postgres")
         cur = conn.cursor()
         # create table one by one
         for command in commands:
