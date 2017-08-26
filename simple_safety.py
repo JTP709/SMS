ddd7b0ee2c23c75624db7113f0be7295674c9193
JTP709 <JTP709@gmail.com>
2017-08-26 09:46:27 -0400

    updated nav menus, finished audit routes, started actions route

--- simple_safety.py
+++ simple_safety.py
@@ -1,7 +1,8 @@
 """
 TODO: main page
-- Dashboard
-	- 
+- Action Item Update
+ - Incident Report "finding" auto populate bug
+ - Add date to action items
 - Generate Report Button
 	- Filter reports
 	- Select reports
@@ -66,6 +67,22 @@ def getCaseID():
 	results_i = int(results[0])
 
 	return str(results_i)
+	db.close()
+
+def getAuditID():
+	db, cursor = connect()
+	query = ("""
+				SELECT id
+					FROM audit
+					ORDER BY id desc
+					LIMIT 1;
+			""")
+
+	cursor.execute(query)
+	results = cursor.fetchone()
+	results_a = int(results[0])
+
+	return str(results_a)
 	db.close()
 
 app = Flask(__name__)
@@ -172,7 +189,7 @@ def newIncident():
 
 		return redirect(url_for('incidents'))
 	else:
-		return render_template('newincidents.html')
+		return render_template('incidents_new.html')
 
 @app.route('/incidents/edit/<int:id>/', methods = ['GET','POST'])
 def editIncident(id):
@@ -204,7 +221,7 @@ def editIncident(id):
 		for j in range(len(action_query)):
 			if action_query[j][0] != '' and action_query[j][0] != None:
 				newActionData = (action_query[j][0],case_id[0])
-				insertAction = ("UPDATE action_items SET "+action_query[j][1]+" = %s WHERE case_num = %s")
+				insertAction = ("UPDATE action_items SET "+action_query[j][1]+" = %s WHERE case_id = %s")
 				cursor.execute(insertAction,newActionData)
 
 		db.commit()
@@ -232,7 +249,7 @@ def editIncident(id):
 		data = (str(id),)
 		cursor.execute(query, data)
 		results = cursor.fetchall()
-		return render_template('incident_edit.html',incidents = results)
+		return render_template('incidents_edit.html',incidents = results)
 		db.close()
 
 @app.route('/incidents/delete/<int:id>/', methods = ['GET','POST'])
@@ -251,37 +268,254 @@ def deleteIncident(id):
 		db.close()
 		return redirect(url_for('incidents'))
 	else:
-		return render_template('incident_delete.html', id = id)
+		return render_template('incidents_delete.html', id = id)
 
 @app.route('/audits/')
 def audits():
-	return "Reports Page for incidents and audits"
+	db, cursor = connect()
+	# need to add date
+	query = """
+    		SELECT a.id,
+    				to_char(a.date_time, 'FMMonth FMDD, YYYY'),
+	    			to_char(a.date_time, 'HH12:MI'), 
+	    			a.type,
+	    			a.ans_1,
+	    			a.ans_2,
+	    			a.ans_3,
+	    			i.id,
+	    			i.finding,
+	    			i.corrective_action
+    			FROM audit as a, action_items as i
+    			WHERE a.id = i.audit_id
+    			ORDER BY a.id desc;
+            """
+	cursor.execute(query)
+	results = cursor.fetchall()
+	health = []
+	for i in results:
+		audit_def = 0.0
+		for j in range(len(i)):
+			if i[j] == False:
+				audit_def = audit_def + 1.0
+		audit_perc = int(float(audit_def/3)*100)
+		health.append(audit_perc)
+		
+	length = len(results)
+
+	return render_template('audits.html',audits = results, length = length, health = health)
+	db.close()
 
 @app.route('/audits/new/', methods = ['GET','POST'])
 def newAudit():
-	return "New Audit Page"
+	if request.method == 'POST':
+		db, cursor = connect()
+		insert = ("""
+				INSERT INTO audit (
+								id,
+								date_time,
+								type,
+								que_1,
+								que_2,
+								que_3,
+								ans_1,
+								ans_2,
+								ans_3
+								)
+					VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
+				""")
+		audit_id = getAuditID()
+		new_id = str(int(audit_id)+1)
+		date_time = request.form['date_time']
+		audit_type = request.form['audit_type']
+		answer_1 = request.form['answer_1']
+		answer_2 = request.form['answer_2']
+		answer_3 = request.form['answer_3']
 
-@app.route('/aduits/edit/<int:id>', methods = ['GET','POST'])
+		if audit_type == 'Behavior':
+			question_1 = 'Was the employee wearing their PPE?'
+			question_2 = 'Does the area have designated locations for all carts, tools, pallets, inventory, and non-inventory items?'
+			question_3 = 'Are all barrels in good condition?'
+		if audit_type == 'Area Organization':
+			question_1 = 'Was the employee using proper lifting techniques?'
+			question_2 = 'Are all carts, pallets, tools, and items in a designated location?'
+			question_3 = 'Are all barrels properly labelled?'
+		if audit_type == 'HAZWASTE':
+			question_1 = "Was the employee's work area clean?"
+			question_2 = 'Is the area clean and free from trip hazards?'
+			question_3 = 'Are the appropriate HAZWASTE items stored in the proper container?'
+
+		data = (new_id, date_time, audit_type, question_1, question_2, question_3, answer_1, answer_2, answer_3)
+
+		cursor.execute(insert, data)
+
+		action_items = ("""
+					INSERT INTO action_items (
+									audit_id,
+									finding,
+									corrective_action
+									)
+						VALUES (%s,%s,%s)
+					""")
+
+		finding = request.form['description']
+		corrective_action = request.form['corrective_action']
+
+		data_a = (new_id, finding, corrective_action)
+
+		cursor.execute(action_items, data_a)
+		db.commit()
+		db.close()
+
+		return redirect(url_for('audits'))
+	else:
+		return render_template('audits_new.html')
+
+@app.route('/audits/edit/<int:id>/', methods = ['GET','POST'])
 def editAudit(id):
-	return "Edit audit page %s" % id
+	if request.method == 'POST':
+		db, cursor = connect()
+		
+		audit_id = (int(id),)
+		date_time = request.form.get('date_time')
+		audit_type = request.form.get('audit_type')
+		question_1 = request.form.get('question_1')
+		question_2 = request.form.get('question_2')
+		question_3 = request.form.get('question_3')
+		answer_1 = request.form.get('ansewr_1')
+		answer_2 = request.form.get('ansewr_2')
+		answer_3 = request.form.get('ansewr_3')
 
-@app.route('/aduits/delete/<int:id>', methods = ['GET','POST'])
+		query = [[date_time,'date_time'],[audit_type,'type'],[question_1,'question_1'],[question_2,'question_2'],[question_3,'question_3'],[answer_1,'answer_1'],[answer_2,'answer_2'],[answer_3,'answer_3']]
+
+		for i in range(len(query)):
+			if query[i][0] != '' and query[i][0] != None:
+				newdata = (query[i][0],audit_id[0])
+				insert = ("UPDATE audit SET "+query[i][1]+" = %s WHERE id = %s")
+				cursor.execute(insert,newdata)
+		
+		finding = request.form.get('description')
+		corrective_action = request.form.get('corrective_action')
+
+		action_query = [[finding,'finding'],[corrective_action,'corrective_action']]
+
+		for j in range(len(action_query)):
+			if action_query[j][0] != '' and action_query[j][0] != None:
+				newActionData = (action_query[j][0],audit_id[0])
+				insertAction = ("UPDATE action_items SET "+action_query[j][1]+" = %s WHERE audit_id = %s")
+				cursor.execute(insertAction,newActionData)
+
+		db.commit()
+		db.close()
+
+		return redirect(url_for('audits'))
+
+	else:
+		db, cursor = connect()
+		
+		query = """
+	    		SELECT a.id,
+    				to_char(a.date_time, 'FMMonth FMDD, YYYY'),
+	    			to_char(a.date_time, 'HH12:MI'), 
+	    			a.type,
+	    			a.que_1,
+	    			a.que_2,
+	    			a.que_3,
+	    			a.ans_1,
+	    			a.ans_2,
+	    			a.ans_3,
+	    			i.id,
+	    			i.finding,
+	    			i.corrective_action
+    			FROM audit as a, action_items as i
+    			WHERE a.id = i.audit_id
+    			ORDER BY a.id desc;
+	            """
+		data = (str(id),)
+		cursor.execute(query, data)
+		results = cursor.fetchall()
+		return render_template('audits_edit.html',audits = results)
+		db.close()
+
+@app.route('/audits/delete/<int:id>/', methods = ['GET','POST'])
 def deleteAudit(id):
-	return "Delete audit page %s" % id
+	if request.method == 'POST':
+		db, cursor = connect()
+		delete = """
+				DELETE FROM action_items
+				WHERE audit_id = %s;
+				DELETE FROM audit
+				WHERE id = %s;
+				"""
+		case = (str(id),str(id))
+		cursor.execute(delete,case)
+		db.commit()
+		db.close()
+		return redirect(url_for('audits'))
+	else:
+		return render_template('audits_delete.html', id = id)
 
 @app.route('/actions/')
 def actions():
-	return "Reports Page for incidents and audits"
+	db, cursor = connect()
+	# need to add date
+	query = """
+    		SELECT id,
+    				case_id,
+    				audit_id,
+    				finding,
+    				corrective_action
+    			FROM action_items
+    			ORDER BY id desc;
+
+            """
+	cursor.execute(query)
+	results = cursor.fetchall()
+			
+	length = len(results)
+
+	return render_template('actions.html',actions = results, length = length)
+	db.close()
 
 @app.route('/actions/new/', methods = ['GET','POST'])
 def newActionItem():
-	return "New Action Item Page"
+	if request.method == 'POST':
+		db, cursor = connect()
+		insert = ("""
+				INSERT INTO action_items (
+								id,
+								finding,
+								corrective_action
+								)
+					VALUES (%s,%s,%s)
+				""")
+		case_id = getCaseID()
+		new_id = str(int(case_id)+1)
+		date_time = request.form['date_time']
+		incident_type = request.form['incident_type']
+		incident_cat = request.form['incident_cat']
+		injury = request.form['injury']
+		property_damage = request.form['property_damage']
+		description = request.form['description']
+		root_cause = request.form['root_cause']
 
-@app.route('/actions/edit/<int:id>', methods = ['GET','POST'])
+		data = (new_id, date_time, incident_type, incident_cat, injury, property_damage, description, root_cause)
+
+		#print(date_time, incident_type, incident_cat, injury, property_damage, description, root_cause)
+
+		cursor.execute(insert, data)
+		
+		db.commit()
+		db.close()
+
+		return redirect(url_for('actions'))
+	else:
+		return render_template('actions_new.html')
+
+@app.route('/actions/edit/<int:id>/', methods = ['GET','POST'])
 def editActionItem(id):
 	return "Edit action item %s" % id
 
-@app.route('/actions/delete/<int:id>', methods = ['GET','POST'])
+@app.route('/actions/delete/<int:id>/', methods = ['GET','POST'])
 def deleteActionItem(id):
 	return "Delete action item %s" % id
 
